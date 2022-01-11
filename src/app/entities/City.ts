3e00767dd0c3ce80989cd274d3eda2e3246e155a
Lucas Dias Coelho <lucas.coelho@compasso.com.br>
2022-01-11 09:42:31 -0300

    feat(entities): create entities for mapping between a typescript class and a database table

---
+++ src/app/entities/City.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from "uuid";
+
+@Entity('cities')
+export class City {
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    state: string;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
+}
---
+++ src/app/entities/Client.ts
@@ -0,0 +1,35 @@
+import { Entity, Column, PrimaryColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { City } from '../entities/City';
+
+@Entity('clients')
+export class Client {
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    sex: string;
+
+    @Column()
+    birthdate: Date;
+
+    @Column()
+    age: number;
+
+    @Column()
+    city_id: string;
+
+    @ManyToOne(() => City)
+    @JoinColumn({ name: 'city_id' })
+    city: City;
+
+
+    constructor() {
+        if(!this.id) {
+            this.id = uuid();
+        }
+    }
+}
