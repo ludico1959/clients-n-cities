109f74cf0c3e0b04100275fc7fd022d0c93a7f2e
Lucas Dias Coelho <ludico1959@gmail.com>
2022-01-16 00:15:38 -0300

    docs(swagger): create swagger

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import 'reflect-metadata';
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from '../swagger.json';
 import '../infra/database';
 import { router } from '../routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
