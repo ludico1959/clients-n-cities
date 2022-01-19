519b3192e5e4bb71d40cdc75bc08bff9b89c3d35
Lucas Dias Coelho <ludico1959@gmail.com>
2022-01-19 09:46:19 -0300

    feat(migrations): add an enum list of gender and state to the migration create clients and create cities tables, respectively

--- src/infra/database/migrations/1641869624287-CreateCities.ts
+++ src/infra/database/migrations/1641869624287-CreateCities.ts
@@ -17,7 +17,36 @@ export class CreateCities1641869624287 implements MigrationInterface {
           },
           {
             name: 'state',
-            type: 'varchar'
+            type: 'varchar',
+            enum: [
+              'AC',
+              'AL',
+              'AP',
+              'AM',
+              'BA',
+              'CE',
+              'DF',
+              'ES',
+              'GO',
+              'MA',
+              'MT',
+              'MS',
+              'MG',
+              'PA',
+              'PB',
+              'PR',
+              'PE',
+              'PI',
+              'RJ',
+              'RN',
+              'RS',
+              'RO',
+              'RR',
+              'SC',
+              'SP',
+              'SE',
+              'TO'
+            ]
           }
         ]
       })
