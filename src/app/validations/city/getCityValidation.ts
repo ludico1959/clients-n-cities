eeaebcd4d17731dfd334e73ad3c29187c6a18f11
Lucas Dias Coelho <ludico1959@gmail.com>
2022-01-21 22:54:51 -0300

    feat(validation): add validation for finding city route

---
+++ src/app/validations/city/getCityValidation.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
+import { brazilianUF } from '../../utils/enumStates';
+
+export = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const entity = Joi.object({
+      name: Joi.string().trim().optional(),
+      state: Joi.string()
+        .trim()
+        .optional()
+        .valid(...Object.keys(brazilianUF))
+    });
+
+    const { error } = entity.validate(req.query, { abortEarly: false });
+
+    if (error) throw error;
+
+    return next();
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
--- src/routes/city.routes.ts
+++ src/routes/city.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { CityController } from '../app/controller/CityController';
 import createCityValidation from '../app/validations/city/createCityValidation';
+import getCityValidation from '../app/validations/city/getCityValidation';
 
 const cityRouter = Router();
 
 cityRouter.post('/api/v1/cities', createCityValidation, new CityController().create);
-cityRouter.get('/api/v1/cities', new CityController().find);
+cityRouter.get('/api/v1/cities', getCityValidation, new CityController().find);
 
 export { cityRouter };
